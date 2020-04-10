c25564fd662d273e28ec872518d6097425fa4fd3
primenumber_nak <DICE2000@users.noreply.github.com>
2020-04-10 13:27:45 +0900

    フロントビューの効果音かぶりの修正等

--- dice2000_YEP_BattleEngineFanPatch.js
+++ dice2000_YEP_BattleEngineFanPatch.js
@@ -9,6 +9,10 @@
 //   エラー音が返るように修正。
 //   味方単体のスキルを戦闘不能者に使おうとする時にもエラー音が返る仕様に。
 //
+// ★フロントビュー時に敵の攻撃開始の効果音が被ってしまうのを修正。一応選択式。
+//
+// ★詳細なバトルログのセンタリングを強制ではなく選択式にした。
+//
 // ----------------------------------------------------------------------------
 // NAK a.k.a. 22番目の素数
 // [Blog]   : http://nak.hits.jp/documents/rpgmaker.html
@@ -20,6 +24,14 @@
  *
  * @help このスクリプトはYEPシリーズの下に置いてください。
  * 
+ * @param Wait Enemy Attack
+ * @text 敵攻撃にウェイトを取る
+ * @desc フロントビュー時、敵の攻撃開始の効果音が聞こえるようウェイトを取ります。
+ * @type boolean
+ * @on ON
+ * @off OFF
+ * @default false
+ * 
  * @param Ignore Miss
  * @text 失敗判定を無視
  * @desc 失敗判定の演出を無視し、回避判定の演出に統一します。
@@ -27,6 +39,21 @@
  * @on ON
  * @off OFF
  * @default false
+ *
+ * @param BattleLogAlign
+ * @text バトルログの行揃え
+ * @desc 詳細なバトルログのテキスト位置を変更します（一部）
+ * @type select
+ * @option 左
+ * @value 
+ * @option 中央
+ * @value <CENTER>
+ * @default 
+ *
+ * @param BattleLogPosY
+ * @text バトルログのY座標
+ * @type number
+ * @default 0
  *
  * @param ResultWindowPos
  * @text 戦闘結果のウィンドウ位置
@@ -38,78 +65,88 @@
  * @option 下
  * @value 2
  * @default 2
- *
- * @param BattleLogPosY
- * @text バトルログのY座標
- * @type number
- * @default 0
- *
+ * 
  */
 
 //グローバルに定義されている変数の存在チェック
 var Imported = Imported || {};
 var Yanfly = Yanfly || {};
 
-var paramIgnoreMiss = (PluginManager.parameters('dice2000_YEP_BattleEngineFanPatch')['Ignore Miss']  === 'true');
-var paramResultWindowPos = Number((PluginManager.parameters('dice2000_YEP_BattleEngineFanPatch')['ResultWindowPos']));
-var paramBattleLogPosY = Number((PluginManager.parameters('dice2000_YEP_BattleEngineFanPatch')['BattleLogPosY']));
+var parameters     = PluginManager.parameters('dice2000_YEP_BattleEngineFanPatch');
+var paramWaitEnemyAttack = (parameters['Wait Enemy Attack']  === 'true');
+var paramIgnoreMiss = (parameters['Ignore Miss']  === 'true');
+var paramBattleLogAlign = String(parameters['BattleLogAlign']);
+var paramBattleLogPosY = Number(parameters['BattleLogPosY']);
+var paramResultWindowPos = Number((parameters['ResultWindowPos']));
 
 if (Imported.YEP_BattleEngineCore) {
 if (Yanfly.BEC.version) {
-    
-    Game_Battler.prototype.performResultEffects = function() {
-        var result = this.result();
-        if(paramIgnoreMiss){
-            if (result.missed || result.evaded) {
-              if (result.physical) {
-                this.performEvasion();
-              } else {
-                this.performMagicEvasion();
-              }
-            }            
-        }else{
-            //元の定義
-            if (result.missed && result.physical) this.performMiss();
-            if (result.evaded) {
-              if (result.physical) {
-                this.performEvasion();
-              } else {
-                this.performMagicEvasion();
-              }
-            }
+
+  var dice2000_Window_BattleLog_showEnemyAttackAnimation = Window_BattleLog.prototype.showEnemyAttackAnimation;
+  Window_BattleLog.prototype.showEnemyAttackAnimation = function(subject, targets) {
+    if(!$gameSystem.isSideView() && paramWaitEnemyAttack){
+      //空白行
+    }else{
+      dice2000_Window_BattleLog_showEnemyAttackAnimation.apply(this, arguments);
+    }
+  };
+
+  var dice2000_Sprite_Enemy_startWhiten = Sprite_Enemy.prototype.startWhiten;
+  Sprite_Enemy.prototype.startWhiten = function() {
+    if(!$gameSystem.isSideView() && paramWaitEnemyAttack){
+      SoundManager.playEnemyAttack();
+      this._effectDuration = 32;
+    }else{
+      dice2000_Sprite_Enemy_startWhiten.apply(this, arguments);
+    }
+	};
+
+  var dice2000_Game_Battler_performResultEffects = Game_Battler.prototype.performResultEffects;
+  Game_Battler.prototype.performResultEffects = function() {
+    var result = this.result();
+    if(paramIgnoreMiss){
+      if (result.missed || result.evaded) {
+        if (result.physical) {
+          this.performEvasion();
+        } else {
+          this.performMagicEvasion();
         }
-        if (result.hpAffected) {
-          if (result.hpDamage > 0 && !result.drain) {
-            this.performDamage();
-          }
-          if (result.hpDamage < 0) {
-            this.performRecovery();
-          }
+      }
+      if (result.hpAffected) {
+        if (result.hpDamage > 0 && !result.drain) {
+          this.performDamage();
         }
-        if (this.isAlive() && result.mpDamage !== 0 && result.mpDamage < 0) {
+        if (result.hpDamage < 0) {
           this.performRecovery();
         }
-        if (this.isAlive() && result.tpDamage !== 0 && result.tpDamage < 0) {
-          this.performRecovery();
-        }
-    };
+      }
+      if (this.isAlive() && result.mpDamage !== 0 && result.mpDamage < 0) {
+        this.performRecovery();
+      }
+      if (this.isAlive() && result.tpDamage !== 0 && result.tpDamage < 0) {
+        this.performRecovery();
+      }
+    }else{
+      dice2000_Game_Battler_performResultEffects.apply(this, arguments);
+    }
+  };
 
     //味方単体のスキルが使用できない時にエラー音を返す
     //BattleCore無関係の改造
     Window_BattleActor.prototype.isOkEnabled = function() {
-        return Window_Selectable.prototype.isOkEnabled.call(this);
+      return Window_Selectable.prototype.isOkEnabled.call(this);
     };
 
     var dice2000_Window_BattleActor_processOk = Window_BattleActor.prototype.processOk;
     Window_BattleActor.prototype.processOk = function() {
-        var action = BattleManager.inputtingAction();
-        if(!action.isForAll() && !this._selectDead && this.actor().isDead()){
-            this.playBuzzerSound();
-        }else if(!action.isForAll() && this._selectDead && !this.actor().isDead()){
-            this.playBuzzerSound();
-        }else{
-            dice2000_Window_BattleActor_processOk.call(this);
-        }
+      var action = BattleManager.inputtingAction();
+      if(!action.isForAll() && !this._selectDead && this.actor().isDead()){
+          this.playBuzzerSound();
+      }else if(!action.isForAll() && this._selectDead && !this.actor().isDead()){
+          this.playBuzzerSound();
+      }else{
+          dice2000_Window_BattleActor_processOk.call(this);
+      }
     };
 
     //戦闘終了メッセージウィンドウ位置
@@ -144,11 +181,12 @@ if (Yanfly.BEC.version) {
         this.y = paramBattleLogPosY;
     };
 
+    //詳細なテキスト表示をセンタリングする    
     Window_BattleLog.prototype.displayCurrentState = function(subject) {
         if (!Yanfly.Param.BECShowStateText) return;
         var stateText = subject.mostImportantStateText();
         if (stateText) {
-            this.push('addText', '<CENTER>' + subject.name() + stateText);
+            this.push('addText', paramBattleLogAlign + subject.name() + stateText);
             this.push('wait');
             this.push('clear');
         }
@@ -164,7 +202,7 @@ if (Yanfly.BEC.version) {
             if (stateMsg) {
                 this.push('popBaseLine');
                 this.push('pushBaseLine');
-                this.push('addText', '<CENTER>' + target.name() + stateMsg);
+                this.push('addText', paramBattleLogAlign + target.name() + stateMsg);
                 this.push('waitForEffect');
             }
         }, this);
@@ -176,10 +214,37 @@ if (Yanfly.BEC.version) {
             if (state.message4) {
                 this.push('popBaseLine');
                 this.push('pushBaseLine');
-                this.push('addText', '<CENTER>' + target.name() + state.message4);
+                this.push('addText', paramBattleLogAlign + target.name() + state.message4);
             }
         }, this);
     };
+
+    Window_BattleLog.prototype.displayCounter = function(target) {
+        if (Yanfly.Param.BECShowCntText) {
+          this.addText(paramBattleLogAlign + TextManager.counterAttack.format(target.name()));
+        }
+        target.performCounter();
+        this.showAttackAnimation(target, [BattleManager._subject]);
+        this.waitForAnimation();
+    };
+
+    Window_BattleLog.prototype.displayReflection = function(target) {
+        if (Yanfly.Param.BECShowRflText) {
+          this.addText(paramBattleLogAlign + TextManager.magicReflection.format(target.name()));
+        }
+        target.performReflection();
+        var animationId = BattleManager._action.item().animationId;
+        this.showNormalAnimation([BattleManager._subject], animationId);
+        this.waitForAnimation();
+    };
+
+    Window_BattleLog.prototype.displaySubstitute = function(substitute, target) {
+        if (Yanfly.Param.BECShowSubText) {
+          var substName = substitute.name();
+          this.addText(paramBattleLogAlign + TextManager.substitute.format(substName, target.name()));
+        }
+        substitute.performSubstitute(target);
+    };    
 
 }   // Yanfly.BEC.version
 };  // YEP_BattleEngineCore
